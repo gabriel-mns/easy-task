e4fdbb041df688fdba80d54feebcc3e1fdf6b396
Gabriel Martins <gabrielgmns071204@gmail.com>
2025-02-08 11:49:56 -0300

    refactor: criado novo módulo para tasks

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,29 +1,23 @@
 import { NgModule } from "@angular/core";
 import { AppComponent } from "./app.component";
-import { HeaderComponent } from "./components/header/header.component";
 import { UserComponent } from "./components/user/user.component";
-import { TasksComponent } from "./components/tasks/tasks.component";
-import { NewTaskComponent } from "./components/tasks/new-task/new-task.component";
-import { TaskComponent } from "./components/tasks/task/task.component";
-import { CardComponent } from "./components/shared/card/card.component";
 import { BrowserModule } from "@angular/platform-browser";
-import { FormsModule } from "@angular/forms";
 import { SharedModule } from "./components/shared/shared.module";
+import { TasksModule } from "./components/tasks/tasks.module";
 
 @NgModule({
     declarations: [
         AppComponent, 
         UserComponent, 
-        TasksComponent, 
-        NewTaskComponent,
-        TaskComponent,
     ],
     imports: [
         BrowserModule,
-        FormsModule,
-        SharedModule
+        SharedModule,
+        TasksModule
     ],
-    bootstrap: [AppComponent]
+    bootstrap: [
+        AppComponent
+    ]
 })
 export class AppModule{
 
